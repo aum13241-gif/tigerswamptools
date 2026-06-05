b664db2eeadeeb723885ce8f405193de5f5f33cc
aum13241-gif <aum.13241@gmail.com>
2026-06-05 09:32:50 +0700

    Add files via upload

---
+++ products.js
@@ -0,0 +1,92 @@
+const milwaukeeProducts = [
+    {
+        id: "m18-gg-0",
+        name: "เครื่องอัดจารบีไร้สาย 18 โวลต์ M18 GG-0",
+        detail: "M18 GG-0",
+        image: "https://down-th.img.susercontent.com/th-11134207-81ztj-mnwmqtr7ffnsb0",
+        price: "11,436", // สามารถกำหนดราคาเพิ่มเองตรงนี้ได้เลยครับ
+        description: "แรงดันสูงสุด 690 บาร์ในระดับที่ใช้ในอุตสาหกรรม"
+    },
+    {
+        id: "m18-fmdp-0c0",
+        name: "สว่านแท่นแม่เหล็กไร้สาย M18 FMDP-0C0",
+        detail: "M18 FMDP-0C0",
+        image: "https://down-th.img.susercontent.com/th-11134207-81ztc-mnwf67pc25fl24",
+        price: "52,715",
+        description: "สว่านกำลังสูง สำหรับงานเจาะและงานอุตสาหกรรม"
+    },
+    {
+        id: "m18-2824-20",
+        name: "เครื่องเป่าเศษใบไม้ไร้สาย 36V M18 2824-20",
+        detail: "M18 2824-20",
+        image: "https://down-th.img.susercontent.com/th-11134207-81ztd-mnxzjqv085511c",
+        price: "12,442",
+        description: "เป็นเครื่องเป่าลมประสิทธิภาพสูงที่ใช้ระบบแบตเตอรี่คู่เหมาะสำหรับการใช้งานหนัก"
+    },
+    {
+        id: "m18-fvp5-0",
+        name: "ปั๊มสุญญากาศ MILWAUKEE M18 FVP5-0",
+        detail: "M18 FVP5-0",
+        image: "https://down-th.img.susercontent.com/th-11134207-81zte-mny1jeoo6w3m7a",
+        price: "23,775",
+        description: "กำจัดสายไฟรุงรัง สร้างสูญญากาศได้เร็วขึ้นด้วยระบบ Manifold Mode"
+    },    
+
+    // ข้างบนคือตัวอย่างหน้าแรก
+
+  {
+    id: "m12-fpd2-0",
+    name: "สว่านกระแทก M12 FUEL™",
+    detail: "M12 FPD2-0",
+
+    image: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCkijYOR6UowdT03N8PSvkyR1Jjyw1ZqIebw&s",
+
+    images: [
+        "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTCkijYOR6UowdT03N8PSvkyR1Jjyw1ZqIebw&s",
+        "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/m12_fpd2_app_3.png?v=202405291424",
+        "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/m12_fpd2_app_2_1.png?v=202405291424    ",
+        "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/m12_fpd2_app_1.png?v=202405291424"
+    ],
+
+    price: "6,343",
+
+    description: "ข้อมูลจำเพาะทางเทคนิค (Specifications)แรงดันไฟฟ้า: 12 โวลต์ (ระบบแบตเตอรี่ M12)แรงบิดสูงสุด: 45.2 นิวตันเมตร (Nm) ให้แรงบิดสูงเกินตัวเมื่อเทียบกับขนาดความเร็วรอบเปล่า (ปรับได้ 2 ระดับ):เกียร์ 1: 0 - 450 รอบต่อนาที (RPM)เกียร์ 2: 0 - 1,550 รอบต่อนาที (RPM)"
+},
+ {
+    id: "m12-fid2-0",
+    name: "ไขควงกระแทก M12 FUEL™",
+    detail: "M12 FID2-0",
+
+    image: "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fid2_hero_1.png",
+
+    images: [
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fid2_hero_1.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fid2_app_1.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fid2_app_2.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fid2_app_3.png"
+    ],
+
+    price: "5,702",
+
+    description: "มอเตอร์ไร้แปรงถ่าน POWERSTATE ให้แรงบิดสูงสุด 170Nm สำหรับไขควงกระแทกขนาดกะทัดรัดที่เร็วขึ้นTri-LED ส่องสว่างพื้นที่ทำงานเพื่อให้มองเห็นพื้นที่ทำงานได้สูงสุดควบคุมการกระแทก 4 โหมดพร้อมโหมดการต๊าปในตัวโดยเฉพาะ"
+},
+{
+    id: "m12-fddxkit-ox",
+    name: "สว่านไขควงพร้อมชุดหัวงานติดตั้ง M12 FUEL™",
+    detail: "M12 FDDXKIT-OX",
+
+    image: "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/milwaukee_installation_drill_driver_m12_fuel_07_png.webp?v=202405291424",
+
+    images: [
+        "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/milwaukee_installation_drill_driver_m12_fuel_07_png.webp?v=202405291424",
+        "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/018365009.jpeg?v=202405291424",
+        "https://image.makewebcdn.com/makeweb/m_1920x0/2dP9r9iWx/DefaultData/08447e3288f3bf28e03872200054d3a9.jpg?v=202405291424",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fid2_app_3.png"
+    ],
+
+    price: "10,805",
+
+    description: "ข้อมูลทางเทคนิคที่สำคัญแรงดันไฟฟ้า: 12 โวลต์ (ทำงานร่วมกับแบตเตอรี่ Milwaukee M12 ได้ทุกรุ่น)มอเตอร์: ไร้แปรงถ่าน POWERSTATE™ Brushless ให้ความแรงสม่ำเสมอ อายุการใช้งานยาวนานแรงบิดสูงสุด (Torque): 34 Nm แรงบิดสูงเกินตัวสำหรับสว่านขนาดเล็กความเร็วรอบ: ปรับได้ 2 ระดับ (0 - 400 และ 0 - 1,600 รอบต่อนาที)ความยาวตัวเครื่อง: สั้นเพียง 130 มิลลิเมตร (เมื่อถอดหัวจับออกแล้วใช้แกนหกเหลี่ยมโดยตรง)ระบบป้องกัน: แผงวงจรอัจฉริยะ REDLINK PLUS™ ป้องกันเครื่องและแบตเตอรี่เสียหายจากการใช้งานเกินกำลังหรือความร้อนสูงฟังก์ชันเสริม: ด้านหน้ามี แถบแม่เหล็กสำหรับดูดจับดอกไขควง สำรอง ช่วยให้หยิบใช้งานได้สะดวกระหว่างทำงาน"
+},
+];
+    
