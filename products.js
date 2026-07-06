93dbacafd4df0ae789bb4a29b755b4608f214713
aum13241-gif <aum.13241@gmail.com>
2026-07-06 09:49:30 +0700

    Add files via upload

--- products.js
+++ products.js
@@ -88,5 +88,96 @@ const milwaukeeProducts = [
 
     description: "ข้อมูลทางเทคนิคที่สำคัญแรงดันไฟฟ้า: 12 โวลต์ (ทำงานร่วมกับแบตเตอรี่ Milwaukee M12 ได้ทุกรุ่น)มอเตอร์: ไร้แปรงถ่าน POWERSTATE™ Brushless ให้ความแรงสม่ำเสมอ อายุการใช้งานยาวนานแรงบิดสูงสุด (Torque): 34 Nm แรงบิดสูงเกินตัวสำหรับสว่านขนาดเล็กความเร็วรอบ: ปรับได้ 2 ระดับ (0 - 400 และ 0 - 1,600 รอบต่อนาที)ความยาวตัวเครื่อง: สั้นเพียง 130 มิลลิเมตร (เมื่อถอดหัวจับออกแล้วใช้แกนหกเหลี่ยมโดยตรง)ระบบป้องกัน: แผงวงจรอัจฉริยะ REDLINK PLUS™ ป้องกันเครื่องและแบตเตอรี่เสียหายจากการใช้งานเกินกำลังหรือความร้อนสูงฟังก์ชันเสริม: ด้านหน้ามี แถบแม่เหล็กสำหรับดูดจับดอกไขควง สำรอง ช่วยให้หยิบใช้งานได้สะดวกระหว่างทำงาน"
 },
+{
+    id: "m12-fqid-o",
+    name: "ไขควงกระแทกระบบไฮดรอลิก M12 FUEL™",
+    detail: "M12 FQID-O",
+
+    image: "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fqid-0-hero01.png",
+
+    images: [
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m12-fqid-0-hero01.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/1/9/19_0373s01-m12-fqid-impact-driver-action-shot.jpg",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/1/9/19_0373s02-m12-fqid-impact-driver-action-shot.jpg",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/1/9/19_0373s03-m12-fqid-impact-driver-action-shot.jpg"
+    ],
+
+    price: "8,040",
+
+    description: "ไขควงกระแทกระบบไฮดรอลิก หัวหกเหลี่ยม M12 FUEL™ SURGE™ เป็นไขควงกระแทก 12V ที่เงียบที่สุดของ MILWAUKEE™ ซึ่งทำงานโดยมีเสียงเบาที่สุดสำหรับ เมื่อเทียบกับไขควงกระแทกเดียวกันที่ มีระดับเสียงที่ 76 dB(A) เท่านั้น สำหรับการใช้งานในการขันไม้ระบบส่งกำลังไฮดรอลิก FLUID-DRIVE™ ลดการสัมผัสระหว่างโลหะภายในเครื่องมือ ทำงานได้เงียบกว่า มีการทำงานที่นุ่มนวลกว่า และมีความทนทานมากกว่าไขควงกระแทกโดยทั่วไปDRIVE CONTROL™ ของ MILWAUKEE™ ช่วยให้ผู้ใช้สามารถเปลี่ยนการตั้งค่าความเร็วและแรงบิดที่แตกต่างกันได้ 4 ระดับ เพื่อให้มีความหลากหลายสูงสุด"
+},
+{
+    id: "c12-rad-ob",
+    name: "สว่านไขควงมุมฉาก M12 FUEL™",
+    detail: "C12 RAD OB",
+
+    image: "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/c/1/c12rad-0-hero01.png",
+
+    images: [
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/c/1/c12rad-0-hero01.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/c/1/c12rad-202-hero01.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/c/1/c12rad-202-hero01.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/c/1/c12rad-202-hero01.png"
+    ],
+
+    price: "4,695",
+
+    description: "เทคโนโลยีแบตเตอรี่ลิเธียมไอออน REDLITHIUM™ ชุดที่ดีเยี่ยมสำหรับงานก่อสร้าง อิเล็กทรอนิกส์ และประสิทธิภาพเพื่อให้การทำงานที่มากกว่าต่อการชาร์จหนึ่งครั้งและตลอดอายุการใช้งานชุดแบตเตอรี่สวิตช์ท้องเครื่องแบบยาว เพื่อความสะดวกสบายไม่ว่างานจะหนักแค่ไหนคลัทช์อิเล็กทรอนิกส์ที่เป็นนวัตกรรม ส่งแรงบิดสูงถึง 12 นิวตันเมตรมาพร้อมกับการตั้งค่าและโหมดการเจาะ 11 แบบเพื่อการควบคุมการใช้งานประเภทขันได้อย่างสมบูรณ์"
+},
+{
+    id: "fpd3-0",
+    name: "สว่านกระแทก M18 FUEL™",
+    detail: "M18 FPD3-0",
+
+    image: "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fpd3_hero-1.png",
+
+    images: [
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fpd3_hero-1.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fpd3_app-1.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fpd3_app-4.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fpd3_app-11.png"
+    ],
+
+    price: "8,793",
+
+    description: "มอเตอร์ไร้แปรงถ่าน POWERSTATE ให้กำลังมากกว่าเพื่อครอบคลุมการใช้งานที่หลากหลายการเจาะและการขันที่เร็วขึ้นภายใต้ภาระการใช้งานหนักหัวจับโลหะ 13 มม. พร้อมฟันคาร์ไบด์เพื่อความทนทาน การจับยึด และแรงยึดดอกไขควงสูงสุด"
+},
+{
+    id: "fid3-0",
+    name: "ไขควงกระแทก M18 FUEL™",
+    detail: "M18 FID3-0",
+
+    image: "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fid3_hero-1.png",
+
+    images: [
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fid3_hero-1.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fid3_app-1.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fid3_app-4.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fid3_app-10.png"
+    ],
+
+    price: "8,162",
+
+    description: "มอเตอร์ไร้แปรงถ่าน POWERSTATE มอบกำลังที่เหนือกว่าเพื่อความสามารถที่ครบครันในการใช้งานที่หลากหลายระบบควบคุมไกปืนขั้นสุดยอด ให้การเร่งความเร็วที่ราบรื่นสำหรับสกรูขนาดเล็ก ป้องกันการสึกหรอของสกรูหรือความเสียหายของวัสดุขนาดกะทัดรัด (114 มม.) เหมาะสำหรับการใช้งานในพื้นที่แคบ"
+},
+{
+    id: "fqid-0",
+    name: "ไขควงกระแทกระบบไฮดรอลิก M18 FUEL™",
+    detail: "M18 FQID-0",
+
+    image: "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18fqid-0-hero01.png",
+
+    images: [
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18fqid-0-hero01.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18fqid-502-hero01.png",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fqid-0-emea-0-hero02.jpg",
+        "https://www.milwaukeetool.in.th/media/catalog/product/cache/207e23213cf636ccdef205098cf3c8a3/m/1/m18-fqid-0-emea-0-hero08.jpg"
+    ],
+    
+
+    price: "9,301",
+
+    description: "มอเตอร์ไร้แปรงถ่าน POWERSTATE™ ของ MILWAUKEE™ มีกำลังมากกว่า เพิ่มระยะเวลาการใช้งานมากขึ้น และยืดอายุการใช้งานที่ยาวนานระบบวงจรอิเล็กทรอนิกส์อัจฉริยะ REDLINK PLUS™ ทำหน้าที่ควบคุมระหว่างแบตเตอรี่และเครื่องมือ เพื่อเพิ่มประสิทธิภาพการทำงาน และการป้องกันความเสียหายได้ดีเยี่ยมแบตเตอรี่ของมิลวอกี้ชนิด REDLITHIUM™-ION ทนทาน แข็งแกร่งมากที่สุดในท้องตลาด สามารถใช้งานได้ต่อเนื่องยาวนาน และยืดอายุการใช้งานแบตเตอรี่มากขึ้น"
+},
 ];
     
